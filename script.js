30dea639c20f17769db1854f6ac404f93155017e
tol-sofia <tol.sofia@yandex.ru>
2019-07-31 11:05:18 +0400

    второе задание

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// @ts-check
+// @ts-nocheck
 
 var number = 23;
 var string = ("двадцать три");
 var boolean = true;
 var noll = null;
 var undefind = undefined;
-alert(number);
-alert(string);
-alert(noll);
-alert(boolean);
-alert(undefined);
+//alert(number);
+//alert(string);
+//alert(noll);
+//alert(boolean);
+//alert(undefined);
 
 typeof car;            // 'number'
 typeof name  // 'string'
@@ -17,16 +17,15 @@ typeof years;          // 'boolean'
 typeof null;          // 'object' (ошибка языка)
 typeof undefined;     // 'undefined'
 
-var car = "Привет!" //1
-alert(car);
-var string = prompt ("Как Вас зовут?");
-var years = prompt ("Сколько вам лет?"); //2
-alert("Вы " + string + " Вам " + years); 
+//var car = "Привет!" //1
+//alert(car);
+//var string = prompt ("Как Вас зовут?");
+//var years = prompt ("Сколько вам лет?"); //2
+//alert("Вы " + string + " Вам " + years); 
 
 var a = prompt ("Введите любое значение");
 // @ts-ignore
-alert(a*a);
+alert (a*a);
 
 //var checked = true; // поле формы помечено галочкой
 //checked = false; 
-
---
+++ script_two.js
@@ -0,0 +1,17 @@
+//console.log (0 == false);
+//console.log (string == null);
+//console.log (null == true);
+//console.log (0 == false);
+
+//alert( true + "test" ); 
+//alert( "123" + undefined );
+
+var x = prompt ("Введите первое число");
+var y = prompt ("Введите второе число");
+if (x > y) {
+    alert ('Первое больше второго')
+} if (x < y) {
+    alert ('Второе больше первого')
+} if (x == y) {
+    alert ('Они равны');
+}
